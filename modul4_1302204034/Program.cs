491f0bc4a4e5941c700f2b39147f5cda6d550d54
jahfal21 <jahfalramadhan4@gmail.com>
2022-03-19 14:27:12 +0700

    merge

--- modul4_1302204034/Program.cs
+++ modul4_1302204034/Program.cs
@@ -10,6 +10,52 @@ namespace modul4_1302204034
     {
         static void Main(string[] args)
         {
+            // Jahfal Mudrik Ramadhan_1302204034
+            SimpleDataBase<double> db = new SimpleDataBase<double>();
+            db.AddNewData(13);
+            db.AddNewData(02);
+            db.AddNewData(22);
+            db.PrintAllData();
+
+            Console.WriteLine(Penjumlahan.JumlahTigaAngka<long>(13, 02, 20));
+        }
+    }
+
+    class SimpleDataBase<T>
+    {
+        private List<T> storedData;
+        private List<DateTime> inputDates;
+
+        public SimpleDataBase()
+        {
+            this.storedData = new List<T>();
+            this.inputDates = new List<DateTime>();
+        }
+
+        public void AddNewData(T newData)
+        {
+            this.inputDates.Add(DateTime.Now);
+            this.storedData.Add(newData);
+        }
+
+        public void PrintAllData()
+        {
+            for (int i = 0; i < this.inputDates.Count; i++)
+            {
+                Console.WriteLine("Data " + i + " berisi: " + this.storedData[i] +
+                    ", yang disimpan pada waktu UTC: " + this.inputDates[i]);
+            }
+        }
+    }
+    class Penjumlahan
+    {
+        public static T JumlahTigaAngka<T>(T input1, T input2, T input3)
+        {
+            dynamic a = input1;
+            dynamic b = input2;
+            dynamic c = input3;
+
+            return a + b + c;
         }
     }
 }
