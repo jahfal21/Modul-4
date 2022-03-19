75285ff744408bac338bf0688284ad6aa8306132
jahfal21 <jahfalramadhan4@gmail.com>
2022-03-19 14:19:42 +0700

    menambahkan class SimpleDateBase

--- modul4_1302204034/Program.cs
+++ modul4_1302204034/Program.cs
@@ -10,6 +10,39 @@ namespace modul4_1302204034
     {
         static void Main(string[] args)
         {
+            // Jahfal Mudrik Ramadhan_1302204034
+            SimpleDataBase<double> db = new SimpleDataBase<double>();
+            db.AddNewData(13);
+            db.AddNewData(02);
+            db.AddNewData(22);
+            db.PrintAllData();
+        }
+    }
+
+    class SimpleDataBase<T>
+    {
+        private List<T> storedData;
+        private List<DateTime> inputDates;
+
+        public SimpleDataBase()
+        {
+            this.storedData = new List<T>();
+            this.inputDates = new List<DateTime>();
+        }
+
+        public void AddNewData(T newData)
+        {
+            this.inputDates.Add(DateTime.Now);
+            this.storedData.Add(newData);
+        }
+
+        public void PrintAllData()
+        {
+            for (int i = 0; i < this.inputDates.Count; i++)
+            {
+                Console.WriteLine("Data " + i + " berisi: " + this.storedData[i] +
+                    ", yang disimpan pada waktu UTC: " + this.inputDates[i]);
+            }
         }
     }
 }
